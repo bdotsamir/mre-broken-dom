e23d2423a6f01b86d93c6eab1d27f7c91d838cb9
bdotsamir <samir.js.zip@gmail.com>
2025-02-09 14:11:31 -0500

    Minimum reproducible environment

---
+++ components/DOMView.tsx
@@ -0,0 +1,13 @@
+"use dom";
+
+// import "mafs/core.css";
+
+// import { Mafs, Coordinates } from "mafs/build/index";
+
+export default function MafsView() {
+  return (
+    <div>
+      <p>Hello DOM!</p>
+    </div>
+  );
+}
